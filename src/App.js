db7c4fbb3ce7f1b8c832a857f5eefbd51e990748
Destiny Boone <destaj.db@gmail.com>
2023-02-23 12:48:40 -0500

    adding a users list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from './Components/User/AddUser';
+import UserList from './Components/User/UserList';
 
 function App() {
   return (
     <div>
       <AddUser></AddUser>
+      <UserList users={[]}></UserList>
     </div>
   );
 }
